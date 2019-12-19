1dc9f586eb8cebcb964ab66c83da1e3e8f242a9c
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2019-12-19 12:55:12 +0530

    Create YetAnotherBallProblem.cpp

---
+++ YetAnotherBallProblem.cpp
@@ -0,0 +1,21 @@
+// url: https://codeforces.com/problemset/problem/1118/E
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+#define pb push_back
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  int n,k; cin>>n>>k;
+
+  if(n > k*(k-1)*1ll ){ std::cout << "NO" << '\n'; return 0 ;}
+  std::cout << "YES" << '\n';
+  int c = 0;
+  for (int i = 0; i < n; i++) {
+    std::cout << i%k+1 << " " << (c+=(i%k == 0)+i%k)%k+1 << '\n';
+  }
+
+  return 0;
+}
