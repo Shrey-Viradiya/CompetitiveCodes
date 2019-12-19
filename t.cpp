c9566d67ff20c8055c69a74864c4be7a4d437401
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2019-12-19 12:55:16 +0530

    Create t.cpp

---
+++ t.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,k;cin>>n>>k;
+	if(n>1ll*k*(k-1)) return puts("No"),0;
+	puts("Yes");int cur=0;
+	for(int i=0;i<n;i++) printf("%d %d\n",i%k+1,((cur+=(i%k==0))+i%k)%k+1);
+}
