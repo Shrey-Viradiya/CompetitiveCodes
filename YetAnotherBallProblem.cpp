56f1441e96450dd7e80c3f6540087e1d9d39a179
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2019-12-19 13:38:17 +0530

    Update YetAnotherBallProblem.cpp

--- YetAnotherBallProblem.cpp
+++ YetAnotherBallProblem.cpp
@@ -10,11 +10,11 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   int n,k; cin>>n>>k;
 
-  if(n > k*(k-1)*1ll ){ std::cout << "NO" << '\n'; return 0 ;}
+  if(n > 1ll*k*(k-1) ){ std::cout << "NO" << '\n'; return 0 ;}
   std::cout << "YES" << '\n';
   int c = 0;
   for (int i = 0; i < n; i++) {
-    std::cout << i%k+1 << " " << (c+=(i%k == 0)+i%k)%k+1 << '\n';
+    std::cout << i%k+1 << " " << ((c+=(i%k == 0))+i%k)%k+1 << '\n';
   }
 
   return 0;
