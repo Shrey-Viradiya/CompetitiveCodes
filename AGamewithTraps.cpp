a90dfd747bbbd3e30210e7733f6949b8702caf75
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2019-12-21 23:49:15 +0530

    Update AGamewithTraps.cpp

--- AGamewithTraps.cpp
+++ AGamewithTraps.cpp
@@ -1,5 +1,6 @@
 // https://codeforces.com/contest/1260/problem/D
 // unsolved // problem in test case 4
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -9,23 +10,24 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  int m,n,k,t;
+  long m,n,k,t;
   cin >> m >> n >> k >> t;
 
-  int tres = t- (n + 1);
-  int tm = tres/2;
+  long tres = t- (n + 1);  // time reserved for person to move with its squad to boss is (n+1)
+                          // tres is applied to diffuse all bombs
+  long tm = tres/2;  // time tm is half of tres because tm time is for going ahead in the grid and tm is to come back to squad
 
   // std::cout << tm << '\n';
 
-  int sol[m] = {0};
+  long sol[m] = {0};
   for (size_t i = 0; i < m; i++) {
     cin>> sol[i];
   }
 
-  int maxd = -1;
+  long long maxd = LLONG_MIN;
 
   for (size_t i = 0; i < k; i++) {
-    int l,r,d;
+    long l,r,d;
     std::cin >> l >> r >>d;
 
     if(r<=tm) continue;
@@ -35,7 +37,7 @@ int main(int argc, char const *argv[]) {
   }
   // std::cout << maxd << '\n';
 
-  int count = 0;
+  long count = 0;
 
   for (size_t i = 0; i < m; i++) {
     if(sol[i] >= maxd)
