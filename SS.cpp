4020b4e44c8b08f22f9e313d5e32958f4644112d
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2019-12-23 23:15:44 +0530

    Update SS.cpp

--- SS.cpp
+++ SS.cpp
@@ -1,13 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <functional>
+#include <thread>
 
 #define pb push_back
 using namespace std;
 
+void fun(string s,string &s2){
+  for (size_t i = 0; i < s.length() - 2; i++) {
+    if(s[i] == s[i+1]){
+      if(s[i+1] == s[i+2]){
+        if(s[i] == 'z'){
+          s[i+1] = 'a';
+          continue;
+        }
+        s[i+1]++;
+        continue;
+      }
+      else{
+      if(s[i] == 'z'){
+        s[i] = 'a';
+        continue;
+      }
+      if(i!=0){
+        s[i] = s[i-1]+1;
+        continue;
+      }else{
+        s[i]++;
+        continue;
+      }
+  }
+    }
+  }
+  s2 = s;
+}
+
 int main(int argc, char const *argv[]) {
   string s;
   std::cin >> s;
+
+  if(s.length()>4)
+  {
+  string s2;
+
+  thread t1(    fun,s.substr(s.length()/2,s.length()),std::ref(s2)  );
+
+  s=s.substr(0,s.length()/2);
 
   for (size_t i = 0; i < s.length() - 2; i++) {
     if(s[i] == s[i+1]){
@@ -35,7 +74,44 @@ int main(int argc, char const *argv[]) {
     }
   }
 
-  std::cout << s << '\n';
+  try{
+    // code
+  }  catch(...){
+    t1.join();
+    throw;
+  }
+
+  t1.join();
+
+  std::cout << s << s2 <<'\n';
+}
+else{
+  for (size_t i = 0; i < s.length() - 2; i++) {
+    if(s[i] == s[i+1]){
+      if(s[i+1] == s[i+2]){
+        if(s[i] == 'z'){
+          s[i+1] = 'a';
+          continue;
+        }
+        s[i+1]++;
+        continue;
+      }
+      else{
+      if(s[i] == 'z'){
+        s[i] = 'a';
+        continue;
+      }
+      if(i!=0){
+        s[i] = s[i-1]+1;
+        continue;
+      }else{
+        s[i]++;
+        continue;
+      }
+  }
+    }
+  }
+}
 
   return 0;
 }
