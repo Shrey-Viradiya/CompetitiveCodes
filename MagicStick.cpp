25680ba0d603f7c4da920a2d33d47bc0c54eef2a
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2019-12-24 17:41:58 +0530

    Update MagicStick.cpp

--- MagicStick.cpp
+++ MagicStick.cpp
@@ -4,9 +4,12 @@ using namespace std;
 int main(){
     int t;cin>>t;
     while(t--){
-        int x,y;cin >> x>>y;
+        long long x,y;cin >> x>>y;
 
-        if((x == 1 && y!=1) || (3*(x-1)/2 == x) ) std::cout << "NO" << '\n';
+        if(x == 1 && y>1) std::cout << "NO" << '\n';
+        else if(x>=4 || x>=y) std::cout << "YES" << '\n';
+        else if((x == 2 || x == 3) && y >3) std::cout << "NO" << '\n';
+        else if((3*(x-1))/2 == x) std::cout << "NO" << '\n';
         else std::cout << "YES" << '\n';
     }
 
